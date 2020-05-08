f1418f677e49015069990741db43796520aa1d72
Alex <alex18812649207@gmail.com>
2020-05-08 20:27:29 +0800

    存入免费课程数据——2020-5-8

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -25,81 +24,83 @@ SECRET_KEY = '1@5ikb1=y58swj)8qc%z&nzwkbx-9h86)6w$vn39pf8v+7bfkk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'Course.apps.CourseConfig',
+	'utils.crossDomain.crossDomain',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	# 'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'BackEnd.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates')]
+		,
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'BackEnd.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
+		'USER': 'root',
+		'PASSWORD': '20001001',
+		'NAME': 'luffycity',
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -114,8 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media配置
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
---
+++ BackEnd/Course/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+
+
+__all__ = ['Category', 'FreeCourse', 'PracticalCourse', 'Teacher', 'CourseDetail', 'CourseChapter', 'CourseSection']
+
+
+# Create your models here.
+class Category(models.Model):
+    """课程分类表"""
+    title = models.CharField(max_length=32, verbose_name="课程的分类")
+    categoryType = models.SmallIntegerField(choices=((0, "免费课"), (1, "实战课")))
+
+    def __str__(self):
+        return self.title + str(self.categoryType)
+
+
+class BaseCourse(models.Model):
+    # 列表+详情页面数据
+    title = models.CharField(max_length=128, verbose_name="课程的名称")
+    courseImage = models.ImageField(upload_to="course/%Y-%m", verbose_name='课程的图片')
+    studyNumber = models.IntegerField(verbose_name="学习人数")
+    hours = models.IntegerField(verbose_name="时长")
+    # 列表页面数据
+    slogan = models.CharField(verbose_name="课程概述", max_length=1024)
+    level = models.SmallIntegerField(choices=((0, '初级'), (1, '中级'), (2, '高级')), default=1)
+    # 其它数据
+    category = models.ForeignKey(to="Category", verbose_name="课程的分类", on_delete=models.PROTECT)
+    courseType = models.SmallIntegerField(choices=((0, "免费课"), (1, "实战课"), (2, "轻课"), (3, "就业班")))
+    courseDetail = models.OneToOneField(to="CourseDetail", on_delete=models.CASCADE)
+    chapter = models.ManyToManyField(to="CourseChapter")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
+
+class FreeCourse(BaseCourse):
+    pass
+
+
+class PracticalCourse(BaseCourse):
+    price = models.IntegerField(verbose_name="价格", default=99)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=32, verbose_name="讲师名字")
+    brief = models.TextField(max_length=1024, verbose_name="讲师介绍")
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+
+    def __str__(self):
+        return self.name
+
+
+class CourseDetail(models.Model):
+    teachers = models.ManyToManyField("Teacher", verbose_name="课程讲师")
+    brief = models.TextField(verbose_name="课程简介")
+    number = models.IntegerField(verbose_name="课程小节", default=0)
+    qqGroup = models.CharField(max_length=11, null=True, blank=True)
+
+    def __str__(self):
+        return self.brief
+
+
+class GoLearn(models.Model):
+    title = models.CharField(verbose_name="将会学到的内容", max_length=256)
+    courseDetail = models.ForeignKey(to="CourseDetail", on_delete=models.PROTECT)
+
+
+class CourseChapter(models.Model):
+    """课程章节表"""
+    chapter = models.SmallIntegerField(verbose_name="第几章")
+    title = models.CharField(max_length=32, verbose_name="课程章节名称")
+
+    def __str__(self):
+        return self.title
+
+
+class CourseSection(models.Model):
+    """课时表"""
+    chapter = models.ForeignKey(to="CourseChapter", on_delete=models.CASCADE)
+    name = models.CharField(max_length=32, verbose_name="课时名称")
+    freeTrail = models.BooleanField(default=False, help_text="是否可试看")
+    sectionType = models.SmallIntegerField(default=2, choices=((0, '文档'), (1, '练习'), (2, '视频')))
+
+    def __str__(self):
+        return self.name
